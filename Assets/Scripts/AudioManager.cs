733e8597a3161bb1efefd793f7e85f058ad91d10
Justin <justin.wade@utdallas.edu>
2022-04-11 12:32:39 -0500

    Sandbox Environment Creation
    Created basic environment / assets  for mechanic testing

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+    public static AudioManager Instance;
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (Instance == null)
+        {
+            // doesn't exist yet, this is now our singleton!
+            Instance = this;
+            // fill references
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+    // Play a single clip through the sound effects source.
+    public void PlaySong(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+            return;
+        s.source.Play();
+    }
+    // Play background music through the music source.
+    void Start()
+    {
+        PlaySong("Theme");
+    }
+}
---
+++ Assets/Scripts/DoorTrigger.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject door;
+    [SerializeField] float openDuration = 3;
+    Collider colliderToDeactivate = null;
+    bool isOpened = false;
+
+    private void Awake()
+    {
+        colliderToDeactivate = GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (isOpened == false)
+        {
+            StartCoroutine(DoorOpen());
+        }
+    }
+    IEnumerator DoorOpen()
+    {
+        // set boolean for detecting lockout
+        isOpened = true;
+
+        ActivateDoor();
+        // simulate this object being disabled. We don't really want to disable it
+        // because we still need script behavior to continue functioning
+        DisableObject();
+
+        // wait for the required duration
+        yield return new WaitForSeconds(openDuration);
+        //reset
+        DeactivateDoor();
+        EnableObject();
+
+        // set boolean to release lockout
+        isOpened = false;
+    }
+    void ActivateDoor()
+    {
+        // open door
+        Debug.Log("Open Door!");
+        door.transform.position += new Vector3(0, 2.4f, 0);
+    }
+    void DeactivateDoor()
+    {
+        // close door
+        Debug.Log("Door Close!");
+        door.transform.position += new Vector3(0, -2.4f, 0);
+    }
+    void DisableObject()
+    {
+        // disable collider, so it can't be retriggered
+        colliderToDeactivate.enabled = false;
+        // disable visuals, to simulate deactivated
+    }
+    void EnableObject()
+    {
+        // enable collider, so it can be retriggered
+        colliderToDeactivate.enabled = true;
+        // enable visuals again, to draw player attention
+    }
+}
